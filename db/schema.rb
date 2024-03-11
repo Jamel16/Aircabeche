df191b7e1e37e4e0483d6d01d2d37df239af0513
claire lenglart <djanetdesign@gmail.com>
2024-03-11 17:30:07 +0100

    fixed users devise

--- db/schema.rb
+++ db/schema.rb
@@ -39,12 +39,18 @@ ActiveRecord::Schema[7.1].define(version: 2024_03_11_151637) do
   end
 
   create_table "users", force: :cascade do |t|
-    t.string "first_name"
-    t.string "last_name"
-    t.string "email"
-    t.string "phone_number"
+    t.string "email", default: "", null: false
+    t.string "encrypted_password", default: "", null: false
+    t.string "reset_password_token"
+    t.datetime "reset_password_sent_at"
+    t.datetime "remember_created_at"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "first_name"
+    t.string "last_name"
+    t.string "phone_number"
+    t.index ["email"], name: "index_users_on_email", unique: true
+    t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
   add_foreign_key "cabeches", "users", column: "users_id"
